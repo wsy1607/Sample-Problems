8deea1f2a814bc3d618af78756687d5027a86785
wsy1607 <swang10@berkeley.edu>
2016-02-03 16:08:30 -0800

    update

--- json/json.py
+++ json/json.py
@@ -1,13 +1,18 @@
 data = {"Header": {"UserID": "ABC", "RawData": {"Transaction": {"Date": "2015-03-03", "Amount": 20.00, "Description": "Burger King"}}}}
 
-def getkeys(data):
-    for key in data.keys():
-        print key
 
-def recu(data):
-    getkeys(data)
+def getkeys(data):
+    keys = []
+    for key in data.keys():
+        keys.append(key)
+    return(keys)
+
+def recu(dictKeys,data):
+    dictKeys += getkeys(data)
     for key in data.keys():
         if type(data[key]) is dict:
-            recu(data[key])
+            recu(dictKeys,data[key])
+    return(dictKeys)
 
-recu(data)
+output = recu([],data)
+print output
