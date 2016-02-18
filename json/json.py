f14408fa1d248c605c9267b52890854bb418618f
wsy1607 <swang10@berkeley.edu>
2016-02-17 16:48:43 -0800

    update

--- json/json.py
+++ json/json.py
@@ -1,5 +1,8 @@
-data = {"Header": {"UserID": "ABC", "RawData": {"Transaction": {"Date": "2015-03-03", "Amount": 20.00, "Description": "Burger King"}}}}
-
+#data = {"Header": {"UserID": "ABC", "RawData": {"Transaction": {"Date": "2015-03-03", "Amount": 20.00, "Description": "Burger King"}}}}
+data11 = {"a11":1}
+data1 = {"a1":data11,"a2":2}
+data3 = {"c1":3,"c2":4}
+data = {"a":data1,"b":2,"c":data3}
 
 def getkeys(data):
     keys = []
@@ -11,8 +14,11 @@ def recu(dictKeys,data):
     dictKeys += getkeys(data)
     for key in data.keys():
         if type(data[key]) is dict:
-            recu(dictKeys,data[key])
+            dictKeys = recu(dictKeys,data[key])
+    #print dictKeys
     return(dictKeys)
 
 output = recu([],data)
+#get unique keys
+output = list(set(output))
 print output
